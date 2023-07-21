c015f8a83a904d969d9f9510b0061f40007ae05f
reidempey <47154278+reidempey@users.noreply.github.com>
2023-07-21 13:49:10 -0600

    Added loops.py

---
+++ Week 2/loops.py
@@ -0,0 +1,19 @@
+
+
+def main():
+    # for i in range(6):
+    #     print(i)
+
+    # names = ["Harry", "Ron", "Hermione"]
+
+    # for name in names:
+    #     print(name, end=" ")
+
+    name = "Harry"
+
+    for char in name:
+        print(char)
+
+
+if __name__ == "__main__":
+    main()
