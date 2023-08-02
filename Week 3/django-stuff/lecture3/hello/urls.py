523878da7ade4cf97ed38fec5ef30ad42d225951
reidempey <47154278+reidempey@users.noreply.github.com>
2023-08-01 18:43:56 -0600

    Updated default route to same as hello app

--- Week 3/django-stuff/lecture3/hello/urls.py
+++ Week 3/django-stuff/lecture3/hello/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    
     path("brian", views.brian, name="brian"),
     path("david", views.david, name="david"),
     path("<str:name>", views.greet, name="greet"),
