56990cf99f852276a2ed0517876d68c941ef2b96
reidempey <47154278+reidempey@users.noreply.github.com>
2023-08-01 18:35:04 -0600

    Modified hello to add responses for routes

--- Week 3/django-stuff/lecture3/hello/views.py
+++ Week 3/django-stuff/lecture3/hello/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, "hello/index.html")
+
+def brian(request):
+    return HttpResponse("Hello, Brian (old function)!")
+
+def david(response):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
