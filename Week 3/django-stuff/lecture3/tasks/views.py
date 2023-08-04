b1776b9c7ce4c2b4a4cc79baa0a34217950b27b3
reidempey <47154278+reidempey@users.noreply.github.com>
2023-08-04 14:15:54 -0600

    Updated the Task app to use sessions to store tasks

--- Week 3/django-stuff/lecture3/tasks/views.py
+++ Week 3/django-stuff/lecture3/tasks/views.py
@@ -1,13 +1,36 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 
-tasks = ['foo', 'bar', 'baz']
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
+
 
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            print(request.session["tasks"])
+            # TQ: Why can't we use a normal append here?
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    return render(request, "tasks/add.html", {
+        "form":NewTaskForm()
+    })
