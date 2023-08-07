e59afaa090b43638aff603d9e95753fce33c589d
reidempey <47154278+reidempey@users.noreply.github.com>
2023-08-07 16:10:14 -0600

    Added a No entry found response

--- Week 3/Problem Set 1/wiki/wiki/encyclopedia/views.py
+++ Week 3/Problem Set 1/wiki/wiki/encyclopedia/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import markdown2
+from . import util
 
 from . import util
 
@@ -8,3 +11,11 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entries(request, entry):
+    # TODO: Fix this so it works with templates
+    if returned_entry := util.get_entry(entry):
+        html_entry = markdown2.markdown(returned_entry)
+        return render(request, html_entry)
+    else:
+        # TQ: If an entry name is typed with the / at the end, it all throws an error...
+        return HttpResponse(f"No {entry} entry found!")
