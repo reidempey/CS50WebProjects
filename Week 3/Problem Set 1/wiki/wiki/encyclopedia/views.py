c4a521170bfc22972d8427bb7116973352de169f
reidempey <47154278+reidempey@users.noreply.github.com>
2023-08-07 16:10:53 -0600

    Added TODO to make entries() work with templates

--- Week 3/Problem Set 1/wiki/wiki/encyclopedia/views.py
+++ Week 3/Problem Set 1/wiki/wiki/encyclopedia/views.py
@@ -17,5 +17,6 @@ def entries(request, entry):
         html_entry = markdown2.markdown(returned_entry)
         return render(request, html_entry)
     else:
+        # TODO: Fix this so it works with templates
         # TQ: If an entry name is typed with the / at the end, it all throws an error...
         return HttpResponse(f"No {entry} entry found!")
