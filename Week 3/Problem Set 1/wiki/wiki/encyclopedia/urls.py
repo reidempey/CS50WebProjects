b24a9775f527e43c221ac461441f62bc426f8245
reidempey <47154278+reidempey@users.noreply.github.com>
2023-08-10 19:54:52 -0600

    Corrected "/" to be at end of "wiki/"

--- Week 3/Problem Set 1/wiki/wiki/encyclopedia/urls.py
+++ Week 3/Problem Set 1/wiki/wiki/encyclopedia/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path("/", views.index, name="index"),
-    path("/<str:entry>", views.entries, name="entries")
+    path("", views.index, name="index"),
+    path("<str:entry>", views.entries, name="entries")
 ]
